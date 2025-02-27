c4ab7efb161f806bb8b2f49c3e8077ceb10c19d2
Enzo Lima <enzolima527@gmail.com>
2025-02-27 10:57:19 -0300

    criação repositório

---
+++ simpleSupport/src/main/java/com/ogl/simpleSupport/SimpleSupportApplication.java
@@ -0,0 +1,13 @@
+package com.ogl.simpleSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSupportApplication.class, args);
+	}
+
+}
---
+++ simpleSupport/src/test/java/com/ogl/simpleSupport/SimpleSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogl.simpleSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
