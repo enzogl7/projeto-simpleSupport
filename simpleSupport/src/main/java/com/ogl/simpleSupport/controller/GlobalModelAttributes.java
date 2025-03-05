f04c6c7210038a9eb760bbe22794c074d70d7392
Enzo Lima <enzolima527@gmail.com>
2025-03-05 11:54:09 -0300

    alteração navbar

---
+++ simpleSupport/src/main/java/com/ogl/simpleSupport/controller/GlobalModelAttributes.java
@@ -0,0 +1,25 @@
+package com.ogl.simpleSupport.controller;
+
+import com.ogl.simpleSupport.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAttributes {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute
+    public void adicionarNomeEmpresaAoModelo(Model model) {
+        var usuarioLogado = userService.getUsuarioLogado();
+
+        if (usuarioLogado != null && usuarioLogado.getEmpresa() != null) {
+            model.addAttribute("nomeEmpresaNavbar", usuarioLogado.getEmpresa().getNome());
+        } else {
+            model.addAttribute("nomeEmpresaNavbar", "");
+        }
+    }
+}
